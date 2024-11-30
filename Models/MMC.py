18e76128505447a5901d1803a606340784bbc612
Khaled Ekramy <khaledekramy195@gmail.com>
2024-11-30 09:35:26 +0200

    starting with MMC model

---
+++ Models/MMC.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import SystemInputs
+
+class MMC:
+    '''
+    '''
+    
+    def __init__(self, sys_inputs: SystemInputs):
+        self._sys_inputs = sys_inputs
+        self._sys_capacity = self._sys_inputs.sys_capacity
+        self._row = self._sys_inputs.arrival_rate / self._sys_inputs.service_rate
+        
+    
